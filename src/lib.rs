5f3fdef0b9c29234c1b6ca7f81187f46989b8522
Elizbeth <elizabethadebayo269@gmail.com>
2024-06-30 18:02:22 +0100

    added algebriac functions to my lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -21,3 +21,17 @@ pub fn divide(a: f64, b: f64) -> Result<f64, &'static str> {
         Ok(a / b)
     }
 }
+
+/// Raises a number to the power of another number.
+pub fn power(base: f64, exponent: f64) -> f64 {
+    base.powf(exponent)
+}
+
+/// Returns the square root of a number.
+pub fn sqrt(number: f64) -> Result<f64, &'static str> {
+    if number < 0.0 {
+        Err("Cannot compute the square root of a negative number")
+    } else {
+        Ok(number.sqrt())
+    }
+}
